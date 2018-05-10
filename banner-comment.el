dcf67d55c16f589235d5c197b8738673a67b4df1
James Ferguson <wjcferguson@gmail.com>
2018-05-10 08:16:25 -0400

    v2.6

--- banner-comment.el
+++ banner-comment.el
@@ -5,7 +5,7 @@
 ;; Author: James Ferguson <james@faff.org>
 ;; URL: https://github.com/WJCFerguson/banner-comment
 ;; Package-Requires: ((emacs "24.4"))
-;; Version: 2.5
+;; Version: 2.6
 ;; Keywords: convenience
 
 ;; This file is not part of GNU Emacs.
